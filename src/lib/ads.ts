df0e4099ca4cb6d80b5d78c20be7fb6a7814faf4
Bclblr <101871576+Bclblr@users.noreply.github.com>
2026-09-18 15:55:23 +0300

    Add web ads consent stub

---
+++ src/lib/ads.ts
@@ -0,0 +1,13 @@
+export function subscribeAdsConsentChanged(_listener: () => void) {
+  return () => undefined;
+}
+
+export function resetAdsInitialization() {}
+
+export async function initializeAds() {
+  return null;
+}
+
+export async function showAdPrivacyOptions() {
+  return false;
+}
